103c40ce06c83b26f22173893ed33f7fa6171001
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2023-01-05 23:07:21 +0530

    feat: add custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter, Montserrat } from '@next/font/google'
+
+const inter = Inter({ subsets: [ "latin" ], variable: '--font-inter' })
+
+const montserrat = Montserrat({ subsets: [ "latin" ], variable: '--font-montserrat' })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} font-main ${inter.variable} font-sub `}>
+      <Component {...pageProps} />
+    </main>)
 }
