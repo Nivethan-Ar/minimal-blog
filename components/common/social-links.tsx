6721790ef5f42616da195044f311416a4dd92200
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2023-01-07 22:26:34 +0530

    feat: add social media links

--- components/common/social-links.tsx
+++ components/common/social-links.tsx
@@ -4,21 +4,31 @@ import { FiMail } from 'react-icons/fi';
 export default function SocialLinks() {
   return (
     <div className='place-self-center relative'>
-      <ul className='fixed top-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col gap-y-8 [&>li>div]:bg-[#555555b5]'>
-        <li className=''>
-          <div className='rounded-full p-3 '>
+      <ul className='fixed top-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col gap-y-8 [&>li]:cursor-pointer [&>li]:bg-[#555555b5]'>
+        <li className='rounded-full p-3'>
+          <a
+            href='https://www.linkedin.com/in/nivethan-me'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <FaLinkedinIn className='text-lg' />
-          </div>
+          </a>
         </li>
-        <li>
-          <div className='rounded-full p-3 '>
+
+        <li className='rounded-full p-3'>
+          <a
+            href='https://github.com/Nivethan-Ar'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <FaGithub className='text-lg' />
-          </div>
+          </a>
         </li>
-        <li>
-          <div className='rounded-full p-3 '>
+
+        <li className='rounded-full p-3'>
+          <a href='mailto:nivethan.me@gmail.com'>
             <FiMail className='text-lg' />
-          </div>
+          </a>
         </li>
       </ul>
     </div>
