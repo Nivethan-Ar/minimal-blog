2f590618d995f5c969b962e762794226eaeda558
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2023-01-08 20:37:37 +0530

    fix: make social icons easily clickable

--- components/common/social-links.tsx
+++ components/common/social-links.tsx
@@ -4,32 +4,34 @@ import { FiMail } from 'react-icons/fi';
 export default function SocialLinks() {
   return (
     <div className='place-self-center relative'>
-      <ul className='fixed top-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col gap-y-8 [&>li]:cursor-pointer [&>li]:bg-[#555555b5]'>
-        <li className='rounded-full p-3'>
-          <a
-            href='https://www.linkedin.com/in/nivethan-me'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedinIn className='text-lg' />
-          </a>
-        </li>
+      <ul className='fixed top-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col gap-y-8 [&>li]:cursor-pointer [&>a>li]:bg-[#555555b5]'>
+        <a
+          href='https://www.linkedin.com/in/nivethan-me'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='group'
+        >
+          <li className='rounded-full p-3'>
+            <FaLinkedinIn className='text-lg text-white/80 group-hover:text-red-600' />
+          </li>
+        </a>
 
-        <li className='rounded-full p-3'>
-          <a
-            href='https://github.com/Nivethan-Ar'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub className='text-lg' />
-          </a>
-        </li>
+        <a
+          href='https://github.com/Nivethan-Ar'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='group'
+        >
+          <li className='rounded-full p-3'>
+            <FaGithub className='text-lg text-white/80 group-hover:text-red-600' />
+          </li>
+        </a>
 
-        <li className='rounded-full p-3'>
-          <a href='mailto:nivethan.me@gmail.com'>
-            <FiMail className='text-lg' />
-          </a>
-        </li>
+        <a className='group' href='mailto:nivethan.me@gmail.com'>
+          <li className='rounded-full p-3'>
+            <FiMail className='text-lg text-white/80 group-hover:text-red-600' />
+          </li>
+        </a>
       </ul>
     </div>
   );
