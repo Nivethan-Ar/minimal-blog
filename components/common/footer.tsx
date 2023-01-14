7cda2bff0673c95f6c7cfb09599c4f78e18aadb9
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2023-01-14 16:09:12 +0530

    fix: add hover color and links

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,11 +1,19 @@
+import Link from 'next/link';
 import { FaHeart } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer className='flex items-center justify-center tracking-wider gap-x-2 mt-6 pb-4 text-xs font-main text-white/50 bg-orange-'>
       &copy; {`${new Date().getFullYear()}`} This page is made with
-      <FaHeart className='animate- text-sm text-white/80' /> by
-      <span className='text-white/80 font-semibold'>Nivethan</span>
+      <Link target={'_blank'} href={'https://github.com/Nivethan-Ar'}>
+        <FaHeart className='animate- text-sm text-white/80 hover:text-red-600' />{' '}
+      </Link>
+      by
+      <Link target={'_blank'} href={'https://www.linkedin.com/in/nivethan-me'}>
+        <span className='text-white/80 font-semibold hover:text-red-600'>
+          Nivethan
+        </span>
+      </Link>
     </footer>
   );
 }
--- components/common/navigation.tsx
+++ components/common/navigation.tsx
@@ -4,13 +4,13 @@ export default function Navigation() {
   return (
     <nav className='place-self-center'>
       <ul className='fixed font top-1/2 -translate-y-1/2 -translate-x-1/2 flex uppercase flex-col gap-y-12'>
-        <li className='hover:underline underline-offset-8'>
+        <li className='hover:text-red-600'>
           <Link href={'/'}>home</Link>
         </li>
-        <li className='hover:underline underline-offset-8'>
+        <li className='hover:text-red-600'>
           <Link href={'/about'}>about</Link>
         </li>
-        <li className='hover:underline underline-offset-8'>
+        <li className=' hover:text-red-600'>
           <Link href={'/blog'}>blog</Link>
         </li>
       </ul>
