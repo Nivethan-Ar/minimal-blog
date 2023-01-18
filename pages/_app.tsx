e38fef70c758bc0eb9a5d165b97d2bdc6ed489de
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2023-01-18 20:30:17 +0530

    fix: add base font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Inter, Lexend, Montserrat } from '@next/font/google'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter, Lexend, Montserrat } from '@next/font/google';
 
-const inter = Inter({ subsets: [ "latin" ], variable: '--font-inter' })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
-const montserrat = Montserrat({ subsets: [ "latin" ], variable: '--font-montserrat' })
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+});
 
-const lexend = Lexend({ subsets: [ 'latin' ],variable:'--font-lexend' })
+const lexend = Lexend({ subsets: ['latin'], variable: '--font-lexend' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${montserrat.variable} font-main ${inter.variable} font-sub ${lexend.variable} font-lex `}>
+    <main
+      className={`${montserrat.variable} font-main ${inter.variable} font-sub ${lexend.variable} font-lex `}
+    >
       <Component {...pageProps} />
-    </main>)
+    </main>
+  );
 }
